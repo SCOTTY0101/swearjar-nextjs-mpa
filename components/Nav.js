e74b553db898c739e422e7c9decc28e1c60394c9
SCOTTY0101 <alan777555@hotmail.com>
2022-08-01 20:40:32 +1000

    nav update

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,8 @@
 //import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import gitIogo from '../public/Images/github.svg';
+//import gitIogo from '../Assets/Images/github.svg';
 import styles from '../styles/Nav.module.css'
 
 
@@ -16,6 +19,11 @@ export default function Nav() {
                     </li>
                     <li>
                         <Link href='/blog'>Blog</Link>
+                    </li>
+                    <li>
+                        <a href="http://www.github.com/SCOTTY0101" target="_blank" rel="noopener noreferrer">
+                        <Image id="gitIcon" src={gitIogo} alt="github click link" width='32' height='32' />
+                        </a>
                     </li>
                 </ul>
             </nav>        
