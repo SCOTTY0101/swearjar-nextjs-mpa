72b81f7946aac159b2becf49ffc78d85f1a40981
SCOTTY0101 <alan777555@hotmail.com>
2022-09-18 20:33:46 +1000

    css changes

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,16 @@
-//import React from 'react'
-import Nav from './Nav';
+import Nav from './Nav'
 import Footer from './Footer'
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.css'
 
 const layout = ({children}) => {
     return (
         <>
         <Nav />
-        <div className={styles.container}>
-            <main className={styles.main}>
-                {children}
-            </main>
-        </div> 
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    {children}
+                </main>
+            </div> 
         <Footer />
         </>
     )
