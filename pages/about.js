b4a284a7e07eb74387882c191bbf917490203375
SCOTTY0101 <alan777555@hotmail.com>
2022-10-02 20:59:39 +1000

    style/typo updates

--- pages/about.js
+++ pages/about.js
@@ -5,15 +5,15 @@ import articleStlye from '../styles/Article.module.css'
 export default function About() {
     return (
         <div>
-         <Head>
-         <title>SwearJar About</title>
-         </Head>
-         <article className={articleStlye.article}>
-         <h1>Second linked page on nextjs.</h1>
-         <p> What SwearJar Software builds.</p> 
-         <p>Re-building the website with Next.js and SSR (Server Side Rendering).</p>
-         <p>Server to run on AWS EC2.</p>
-         </article>
+            <Head>
+                <title>SwearJar About</title>
+            </Head>
+            <article className={articleStlye.article}>
+                <h1>Second linked page on nextjs.</h1>
+                <p> What SwearJar Software builds.</p> 
+                <p>Re-building the website with Next.js and SSR (Server Side Rendering).</p>
+                <p>Server to run on AWS EC2.</p>
+            </article>
         </div>
     )
 }
