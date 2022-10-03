46d4c522a64a0ddeda4dadfa7856578daa3dcc06
SCOTTY0101 <alan777555@hotmail.com>
2022-10-03 21:01:07 +1000

    display changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,21 @@
 import Image from "next/image"
 import footerStyle from '../styles/Footer.module.css'
-import gitIogo from '../public/Images/github.svg';
+import gitLogo from '../public/Images/github.svg'
+import SJnavLogo from '../public/Images/favicon.ico'
 
 
 export default function footer() {
 
   return (
       <footer className={footerStyle.footer}>
-        <p className={footerStyle.p}>Click GitHub icon to view code.</p>
-        <a className={footerStyle.a} href="http://www.github.com/SCOTTY0101/swearjar-nextjs-mpa" target="_blank" rel="noopener noreferrer">
-        <Image id="gitIogo" src={gitIogo} alt="github click link" height='32' width='32'/></a>
-        <p className={footerStyle.p}> &copy; SwearJarSoftware 2019-2022</p>
+        <p className={footerStyle.p}>Click the GitHub icon to view code.</p>
+          <a className={footerStyle.a} href="http://www.github.com/SCOTTY0101/swearjar-nextjs-mpa" target="_blank" rel="noopener noreferrer">
+            <Image className={footerStyle} src={gitLogo} alt="github click link" height='32' width='32'/></a>
+          <div className={footerStyle.copyDiv}>
+            <p> &copy; SwearJarSoftware 2019-2022
+              <Image className={footerStyle.sjLogo} src={SJnavLogo} alt="sJs" width='16' height='16'/> 
+            </p>
+          </div>
       </footer>
   )
 }
