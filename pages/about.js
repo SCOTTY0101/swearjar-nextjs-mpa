836ba747958bfd090a1ea214ef3c5d5f4ad6a28a
SCOTTY0101 <alan777555@hotmail.com>
2022-10-12 21:44:53 +1000

    About.js page changes

--- pages/about.js
+++ pages/about.js
@@ -10,10 +10,10 @@ export default function About() {
             </Head>
             <article className={aboutStyles.article}>
                 
-                <h1>Second linked page on nextjs.</h1>
+                <h1>Second linked page using Nextjs.</h1>
                 <p>What SwearJar Software builds.</p>
                 <p>Re-building the website with Next.js and SSR (Server Side Rendering).</p>
-                <p>Powered by Next.js. The React framework for production.</p>
+                <p>Powered by Nextjs. The React framework for production.</p>
     
             </article>            
         </div>
