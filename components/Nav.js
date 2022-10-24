1931f4cdef8f8e75a161a86eca4b1ddd51d0d01c
SCOTTY0101 <alan777555@hotmail.com>
2022-10-24 21:33:47 +1000

    testing nav

--- components/Nav.js
+++ components/Nav.js
@@ -12,32 +12,34 @@ import SJnavLogo from '../public/Images/favicon.ico';
 export default function Nav() {
     return (
         <div>
-            <nav className='flex'>
+            <nav className='flex  sm:md:lg:xl:2xl:containe w-screen'>
              {/* <Image className='ml-4' src={SJnavLogo} alt="sJs" width='24' height='16'/> */}
                 <ul 
-                className='md:container h-16 justify-center inline-flex items-center'>
-                    <li>
-                    <Image className='justify-center items-center  text-center m-16'  src={SJnavLogo} alt="sJs" width='24' height='16'/>     
-                    </li>            
-                    <li className='items-center m-16'>
+                className='sm:md:lg:xl:2xl:container h-8 m-4 justify-center inline-flex items-center'>
+                    <div className='flex justify-center items-center  text-center'>
+                    <Image  src={SJnavLogo} alt="sJs" width='24' height='24'/>     
+                    </div>            
+                    <li className='items-center mx-8'>
                         <Link href='/'>Home</Link>
                     </li>
-                    <li className='items-center m-16'>
+                    <li className='items-center mx-8'>
                         <Link href='/about'>About</Link>
                     </li>
-                    <li className='items-center m-16'>
+                    <li className='items-center mx-8'>
                         <Link href='/blog'>Blog</Link>
                     </li>
-                    <li className='items-center m-16'>
+                    <li className='items-center mx-8'>
                         <Link href='/divPage'>Div</Link>
                     </li>
-                    <li className='items-center m-16'>
-                        <a href="http://www.linkedin.com/in/alan-george-829706a3" target="_blank" rel="noopener noreferrer">
+                    <li className='items-center mx-8'>
+                        <a className=' flex justify-center items-center  text-center'
+                        href="http://www.linkedin.com/in/alan-george-829706a3" target="_blank" rel="noopener noreferrer">
                         <Image src={linkedinLogo} alt="LinkedIn logo"/>
                         </a>
                     </li>
-                    <li className='items-center m-16'>
-                        <a href="http://www.github.com/SCOTTY0101" target="_blank" rel="noopener noreferrer">
+                    <li className='items-center mx-8'>
+                        <a className=' flex justify-center items-center  text-center'
+                        href="http://www.github.com/SCOTTY0101" target="_blank" rel="noopener noreferrer">
                         <Image id="gitIcon" src={gitIogo} alt="github click link"/>
                         </a>
                     </li>
