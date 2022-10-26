8a89949e152436680297a28c799c262140e0fba5
SCOTTY0101 <alan777555@hotmail.com>
2022-10-26 21:32:02 +1000

    nav changes

--- components/Nav.js
+++ components/Nav.js
@@ -13,7 +13,7 @@ export default function Nav() {
     return (
         <div>
             <nav className='container mx-auto flex flew-wrap justify-center items-center h-16  
-            bg-gray-200 md:rounded-lg md:border-solid'>
+            bg-gray-200 '>
              {/* <Image className='ml-4' src={SJnavLogo} alt="sJs" width='24' height='16'/> */}
                 <div className='flex justify-start items-center h-16 w-16'>
                     <Image  src={SJnavLogo} alt="sJs" width='24' height='24'/>     
@@ -26,19 +26,19 @@ export default function Nav() {
                     <li className='items-center mx-16 hover:underline'>
                         <Link href='/about'>About</Link>
                     </li>
-                    <li className='items-center mx-16 hover:underline'>
+                    <li className='items-center  mx-16 hover:underline'>
                         <Link href='/blog'>Blog</Link>
                     </li>
                     <li className='items-center mx-16 hover:underline'>
                         <Link href='/divPage'>Div</Link>
                     </li>
-                    <li className='items-center mx-16 hover:underline'>
+                    <li className='items-center p-4 mx-16 hover:underline'>
                         <a className=' flex justify-center items-center hover:opacity-70'
                             href="http://www.linkedin.com/in/alan-george-829706a3" target="_blank" rel="noopener noreferrer">
                             <Image src={linkedinLogo} alt="LinkedIn logo"/>
                         </a>
                     </li>
-                    <li className='items-center mx-16'>
+                    <li className='items-center p-4 mx-16'>
                         <a className='flex justify-center items-center hover:opacity-70'
                             href="http://www.github.com/SCOTTY0101" target="_blank" rel="noopener noreferrer">
                             <Image id="gitIcon" src={gitIogo} alt="github click link"/>
