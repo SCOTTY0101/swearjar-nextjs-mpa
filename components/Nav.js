5acd3d9fbd2cf02b0b2824e2fc130d918bd21cae
SCOTTY0101 <alan777555@hotmail.com>
2022-10-27 21:54:20 +1000

    nav changes

--- components/Nav.js
+++ components/Nav.js
@@ -12,40 +12,40 @@ import SJnavLogo from '../public/Images/favicon.ico';
 export default function Nav() {
     return (
         <div>
-            <nav className='container mx-auto flex flew-wrap justify-center items-center h-16  
-            bg-gray-200 '>
-             {/* <Image className='ml-4' src={SJnavLogo} alt="sJs" width='24' height='16'/> */}
-                <div className='flex justify-start items-center h-16 w-16'>
-                    <Image  src={SJnavLogo} alt="sJs" width='24' height='24'/>     
-                </div>  
-                <ul 
-                className='flex flex-wrap justify-center items-center'>          
-                    <li className='items-center mx-16 border-red-400 hover:underline'>
-                        <Link href='/'>Home</Link>
+            <nav className='bg-gray-200 shadow-lg'>
+            <div className="container flex flex-wrap justify-between items-center mx-auto">
+
+                <ul className='sm:self-center inline-flex'>  
+                    <div className=''>
+                        <Image  src={SJnavLogo} alt="sJs" width='24' height='24'/>     
+                    </div>          
+                    <li className=''>
+                        <Link href='/' className=''>Home</Link>
                     </li>
-                    <li className='items-center mx-16 hover:underline'>
+                    <li className=''>
                         <Link href='/about'>About</Link>
                     </li>
-                    <li className='items-center  mx-16 hover:underline'>
+                    <li className=''>
                         <Link href='/blog'>Blog</Link>
                     </li>
-                    <li className='items-center mx-16 hover:underline'>
+                    <li className=''>
                         <Link href='/divPage'>Div</Link>
                     </li>
-                    <li className='items-center p-4 mx-16 hover:underline'>
-                        <a className=' flex justify-center items-center hover:opacity-70'
+                    <li className=''>
+                        <a className=''
                             href="http://www.linkedin.com/in/alan-george-829706a3" target="_blank" rel="noopener noreferrer">
                             <Image src={linkedinLogo} alt="LinkedIn logo"/>
                         </a>
                     </li>
-                    <li className='items-center p-4 mx-16'>
-                        <a className='flex justify-center items-center hover:opacity-70'
+                    <li className=''>
+                        <a className=''
                             href="http://www.github.com/SCOTTY0101" target="_blank" rel="noopener noreferrer">
                             <Image id="gitIcon" src={gitIogo} alt="github click link"/>
                         </a>
                     </li>
 
                 </ul>
+            </div>
             </nav>                
         </div>
     )
